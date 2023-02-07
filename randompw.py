a83d0fa0d412ed1a51a57fb2ff3307d0c4ef4f27
jayanto86 <71925604+jayanto86@users.noreply.github.com>
2023-02-07 12:14:42 +0530

    Create randompw.py

---
+++ randompw.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import random
+
+l = "abcdefghijklmnopqrstuvwxyz"
+u = l.upper()
+n = "0123456789"
+s = "+=-_?/!@#$"
+
+all = l+u+n+s
+len = 6
+passw = "".join(random.sample(all,len))
+print("Your Random Generated password is : "+passw)
+
+
+# In[ ]:
+
+
+
